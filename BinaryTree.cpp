1b4a4a098fcd172b0ad199a73c10650ac64cf97f
Mohit-Jain-git <mohitjainmj2000@gmail.com>
2021-05-15 16:09:58 +0530

    m

---
+++ BinaryTree.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct node
+{
+  int data;
+  struct node *left;
+  struct node *right;
+  node(int val)
+  {
+    data = val;
+    left = NULL;
+    right = NULL;
+  }
+};
+int main()
+{
+  struct node *root = new node(1);
+  root->left = new node(2);
+  root->left->left = new node(4);
+  root->left->right = new node(5);
+  root->right = new node(3);
+  root->right->left = new node(6);
+  root->right->right = new node(7);
+  return 0;
+}
