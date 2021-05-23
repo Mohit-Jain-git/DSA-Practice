4789d126fd0e4df5c190f4b98b39648d93df43bb
Mohit-Jain-git <mohitjainmj2000@gmail.com>
2021-05-23 19:26:05 +0530

    CheckforBST

---
+++ checkforBST.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct node
+{
+  int data;
+  struct node *left;
+  struct node *right;
+  node(int val)
+  {
+    data = val;
+    left = NULL;
+    right = NULL;
+  }
+};
+bool isBST(node *root, node *min, node *max)
+{
+  if (root == NULL)
+    return true;
+  if (min != NULL && root->data <= min->data)
+    return true;
+  if (max != NULL && root->data >= max->data)
+    return true;
+  bool leftValid = isBST(root->left, min, root);
+  bool rightValid = isBST(root->right, root, max);
+  return leftValid and rightValid;
+}
+int main()
+{
+  node *root = new node(4);
+  root->left = new node(2);
+  root->right = new node(5);
+  root->left->left = new node(1);
+  root->left->right = new node(3);
+  root->right->right = new node(6);
+  if (isBST(root, NULL, NULL))
+    cout << "Valid BST";
+  else
+    cout << "Invalid BST";
+  return 0;
+}
