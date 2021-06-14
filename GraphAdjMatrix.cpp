317ea12f9abcbbae62656ef2e654d83b83b7e41f
Mohit-Jain-git <mohitjainmj2000@gmail.com>
2021-06-14 11:27:22 +0530

    AdjMatrix

---
+++ GraphAdjMatrix.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define vi vector<int>
+#define vvi vector<vi>
+#define pii pair<int,int>
+#define vii vector<pii>
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define ff first
+#define ss second 
+const int N = 1e5 + 2;
+const int MOD = 1e9 + 7;
+signed main()
+{ 
+    int n,m;
+    cin>>n>>m;
+    vvi adjm(n+1,vi(n+1,0));
+    // int adjm[100][100];
+    rep(i,0,m)
+    { 
+       int x,y;
+       cin>>x>>y;
+       adjm[x][y] = 1;
+       adjm[y][x] = 1;
+    }
+    cout<<"adjacency matrix of above fraph is given by:"<<endl;
+    rep(i,1,n+1)
+    { 
+       rep(j,1,n+1)
+        cout<<adjm[i][j]<<" ";
+        cout<<endl;
+    }
+    return 0;
+}
