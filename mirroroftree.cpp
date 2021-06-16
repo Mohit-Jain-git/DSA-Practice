01ecea1d0bcf38735775feba56b772525bf9b4ec
Mohit-Jain-git <mohitjainmj2000@gmail.com>
2021-06-16 16:32:02 +0530

    MirrorOfTree

---
+++ mirroroftree.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct node
+{
+  int data;
+  struct node *left;
+  struct node *right;
+  node(int val)
+  {
+    data = val;
+    left = NULL;
+    right = NULL;
+  }
+};
+void mirror(node *root)
+{ 
+   if(root==NULL)
+   return;
+   mirror(root->left);
+   mirror(root->right);
+   swap(root->left,root->right);
+}
+void inorder(node *root)
+{ 
+   if(root==NULL)
+   return;
+   inorder(root->left);
+   cout<<root->data<<" ";
+   inorder(root->right);
+}
+int main()
+{
+  node *root = new node(1);
+  root->left = new node(2);
+  root->right = new node(3);
+  root->left->left = new node(4);
+  root->left->right = new node(5);
+  root->right->left = new node(6);
+  root->right->right = new node(7);
+  inorder(root);
+  cout<<endl;
+  mirror(root);
+  cout<<endl;
+  inorder(root);
+  return 0;
+}
