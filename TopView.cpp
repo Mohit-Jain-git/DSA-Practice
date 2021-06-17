a61b998ec72e248b2728f00d1b69f49d8013869b
Mohit-Jain-git <mohitjainmj2000@gmail.com>
2021-06-17 11:19:01 +0530

    TopView

---
+++ TopView.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct node
+{
+  int data;
+  struct node *left;
+  struct node *right;
+  node(int val)
+  {
+    data = val;
+    left = NULL;
+    right = NULL;
+  }
+};
+void topview(node *root)
+{ 
+   map<int,int> m;
+   queue<pair<node*,int>> q;
+   if(root==NULL)
+   return;
+   q.push({root,0});
+   while(!q.empty())
+   { 
+       node *t = q.front().first;
+       int h = q.front().second;
+       q.pop();
+       if(!m[h])
+       {
+       m[h] = t->data;
+       }
+       if(t->left)
+       q.push({t->left,h-1});
+       if(t->right)
+       q.push({t->right,h+1});
+   }
+     for(auto x:m)
+     cout<<x.second<<" ";
+}
+int main()
+{
+  node *root = new node(1);
+  root->left = new node(2);
+  root->right = new node(3);
+  root->left->left = new node(4);
+  root->left->right = new node(5);
+  root->right->left = new node(6);
+  root->right->right = new node(7);
+  topview(root);
+
+  return 0;
+}
