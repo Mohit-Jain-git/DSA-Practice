3c0e1346325a7aa15b88a26e26e654730090a3b4
Mohit-Jain-git <mohitjainmj2000@gmail.com>
2021-07-03 08:22:15 +0530

    Kadanes algo

--- kadanesalgorithm.cpp
+++ kadanesalgorithm.cpp
@@ -1,26 +1,19 @@
-// C++ program to print largest contiguous array sum
-#include<iostream>
-#include<climits>
+#include<bits/stdc++.h>
 using namespace std;
-
-int maxSubArraySum(int a[], int size)
-{
-	int curr = a[0], maxt = a[0];
-
-	for (int i = 0; i < size; i++)
-	{
-		  curr = max(curr, curr+a[i]);
-        maxt = max(curr,maxt);
-	}
-	return maxt;
-}
-
-/*Driver program to test maxSubArraySum*/
 int main()
-{
-	int a[] = {-2, -3, 4, -1, -2, 1, 5, -3};
-	int n = sizeof(a)/sizeof(a[0]);
-	int max_sum = maxSubArraySum(a, n);
-	cout << "Maximum contiguous sum is " << max_sum;
-	return 0;
+{ 
+	 int n;
+	 cin>>n;
+	 int a[n];
+	 int i;
+	 for(i=0;i<n;i++)
+	  cin>>a[i];
+		int curr=a[0],maxt=a[0];
+		for(i=1;i<n;i++)
+		{ 
+			 curr = max(a[i],curr+a[i]);
+			 maxt = max(curr,maxt);
+		}
+		cout<<maxt<<endl;
+		return 0;
 }
