6701037e51869561197ab607f2280a243cb5c9d0
Mohit-Jain-git <mohitjainmj2000@gmail.com>
2021-07-06 00:11:32 +0530

    Pattern Questions

---
+++ RectanglePattern.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int l,b;
+    cin>>l>>b;
+    for(int i=1;i<=l;i++)
+    { 
+       for(int j=1;j<=b;j++)
+       { 
+          cout<<"* ";
+       }
+       cout<<endl;
+    } 
+    return 0;
+}
