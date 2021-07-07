d03312dc458e2a6ea79471300568212eed4b55a8
Mohit-Jain-git <mohitjainmj2000@gmail.com>
2021-07-07 23:46:22 +0530

    BubbleSort

---
+++ BubbleSort.cpp
@@ -0,0 +1,25 @@
+// Repeatedly swap two adjacent elements if they are in wrong order.
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int n;
+  cin >> n;
+  int a[n];
+  for (int i = 0; i < n; i++)
+    cin >> a[i];
+  int counter = 1;
+  while (counter < n)
+  {
+    for (int i = 0; i < n - counter; i++)
+    {
+      if (a[i] > a[i + 1])
+        swap(a[i], a[i + 1]);
+    }
+    counter++;
+  }
+  for (int i = 0; i < n; i++)
+    cout << a[i] << " ";
+  return 0;
+}
