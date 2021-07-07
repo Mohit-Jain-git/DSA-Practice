515688f8ba1ad26e4a33eab2a381c6e8ff70bcf9
Mohit-Jain-git <mohitjainmj2000@gmail.com>
2021-07-07 23:25:04 +0530

    SelectionSort

---
+++ SelectionSort.cpp
@@ -0,0 +1,28 @@
+// We have to find the min element int the array and swap it with the first element.
+// and do it till our array is sorted.
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int n;
+  cin >> n;
+  int a[n];
+  for (int i = 0; i < n; i++)
+    cin >> a[i];
+  for (int i = 0; i < n - 1; i++)
+  {
+    for (int j = i + 1; j < n; j++)
+    {
+      if (a[i] > a[j])
+      {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+      }
+    }
+  }
+  for (int i = 0; i < n; i++)
+    cout << a[i] << " ";
+  return 0;
+}
