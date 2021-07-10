9b53b0b07c0306a8d508b2dd38021be3ff1d612f
Mohit-Jain-git <mohitjainmj2000@gmail.com>
2021-07-10 08:20:02 +0530

    MaxSubarraySum

---
+++ MaxSubarraySum.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int n, sum = 0, mx = INT_MIN;
+  cin >> n;
+  int a[n];
+  for (int i = 0; i < n; i++)
+  {
+    cin >> a[i];
+  }
+  for (int i = 0; i < n; i++)
+  {
+    sum = 0;
+    for (int j = i; j < n; j++)
+    {
+      sum += a[j];
+    }
+    mx = max(mx, sum);
+  }
+  cout << mx << endl;
+  return 0;
+}
