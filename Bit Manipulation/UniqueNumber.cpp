a4cf3a851e6ce49d626800f5f453796895aeb9dd
Mohit-Jain-git <mohitjainmj2000@gmail.com>
2021-07-13 14:58:21 +0530

    UniqueNumber

---
+++ Bit Manipulation/UniqueNumber.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int UniqueNumber(int a[], int n)
+{
+  int xorsum = 0;
+  for (int i = 0; i < n; i++)
+  {
+    xorsum ^= a[i];
+  }
+  return xorsum;
+}
+int main()
+{
+  int n;
+  cin >> n;
+  int a[n];
+  for (int i = 0; i < n; i++)
+    cin >> a[i];
+  cout << UniqueNumber(a, n);
+  return 0;
+}
