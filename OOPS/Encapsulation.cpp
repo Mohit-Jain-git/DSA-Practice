24d3826e9bdff8161a3749d10265f448f8ead638
Mohit-Jain-git <mohitjainmj2000@gmail.com>
2021-08-08 22:05:01 +0530

    Encapsulation

---
+++ OOPS/Encapsulation.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+class A
+{
+public:
+  int a;
+  void funcA()
+  {
+    cout << "Func A" << endl;
+  }
+
+private:
+  int b;
+  void funcB()
+  {
+    cout << "Func B" << endl;
+  }
+
+protected:
+  int c;
+  void funcC()
+  {
+    cout << "Func C" << endl;
+  }
+};
+int main()
+{
+  A obj;
+  obj.funcA();
+  // obj.funcB();
+  return 0;
+}
